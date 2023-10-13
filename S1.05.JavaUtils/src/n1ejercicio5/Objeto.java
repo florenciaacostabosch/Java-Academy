4c75eae6f089127b9ade1319978c5c729c8e2a6e
florenciaacostabosch <florencia.acostabosch@gmail.com>
2023-10-13 10:12:43 +0200

    commit

---
+++ S1.05.JavaUtils/src/n1ejercicio5/Objeto.java
@@ -0,0 +1,28 @@
+package n1ejercicio5;
+import java.io.Serializable;
+
+	public class Objeto implements Serializable {
+		private String nombre;
+	    private int edad;
+
+	    public Objeto(String nombre, int edad) {
+	        this.nombre = nombre;
+	        this.edad = edad;
+	    }
+
+	    public String getNombre() {
+	        return nombre;
+	    }
+
+	    public void setNombre(String nombre) {
+	        this.nombre = nombre;
+	    }
+
+	    public int getEdad() {
+	        return edad;
+	    }
+
+	    public void setEdad(int edad) {
+	        this.edad = edad;
+	    }
+}
--- S1.05.JavaUtils/src/n1ejercicio5/SerializarObjeto.java
+++ S1.05.JavaUtils/src/n1ejercicio5/SerializarObjeto.java
@@ -1,109 +1,37 @@
 package n1ejercicio5;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Date;
-import java.io.OutputStreamWriter;
 import java.io.Serializable;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-
 public class SerializarObjeto implements Serializable {
-    private int numero;
-    private String cadena;
-	
-    public SerializarObjeto(int numero, String cadena) {
-    	this.numero = numero;
-        this.cadena = cadena;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getCadena() {
-        return cadena;
-    }
-
-    public void setCadena(String cadena) {
-        this.cadena = cadena;
-    }
-    
-public static void main(String[] args) {
-    SerializarObjeto objeto = new SerializarObjeto(25, "Hola");
-	
+	public static void main(String[] args) {
+        // Serializar un objeto
+        Objeto objeto = new Objeto("Juan", 25);
+        try {
+        	FileOutputStream archivoSalida = new FileOutputStream("objeto.ser");
+            ObjectOutputStream salidaObjeto = new ObjectOutputStream(archivoSalida);
+            salidaObjeto.writeObject(objeto);
+            salidaObjeto.close();
+            archivoSalida.close();
+            System.out.println("Objeto serializado en objeto.ser");
+            } catch (Exception e) {
+            	System.out.println("Error al leer el archivo: " + e.getMessage());
+            	}
+        
+        // Deserializar un objeto
+        Objeto objetoDeserializado = null;
+        try {
+            FileInputStream archivoEntrada = new FileInputStream("miobjeto.ser");
+            ObjectInputStream entradaObjeto = new ObjectInputStream(archivoEntrada);
+            objetoDeserializado = (Objeto) entradaObjeto.readObject();
+            entradaObjeto.close();
+            archivoEntrada.close();
+            System.out.println("Objeto deserializado desde miobjeto.ser");
+            } catch (Exception e) {
+        	System.out.println("Error al leer el archivo: " + e.getMessage());
+        	} 
+        }
 }
-	static void serializar(SerializarObjeto objeto) {
-		try (FileOutputStream archivoSalida = new FileOutputStream("miobjeto.ser");
-				ObjectOutputStream objetoSalida = new ObjectOutputStream(archivoSalida)) {
-			objetoSalida.writeObject(objeto);
-			System.out.println("Objeto serializado y guardado en miobjeto.ser");
-			} catch (Exception e) {
-				System.out.println("Error al leer el archivo: " + e.getMessage());
-       }
-	}
-	
-	static void deserializacion(SerializarObjeto objeto){
-		try (FileInputStream archivoEntrada = new FileInputStream("miobjeto.ser");
-		ObjectInputStream objetoEntrada = new ObjectInputStream(archivoEntrada)) {
-			SerializarObjeto miObjeto = (SerializarObjeto) objetoEntrada.readObject();
-			System.out.println("Objeto deserializado: Numero=" + objeto.getNumero() + ", Cadena=" + objeto.getCadena());
-			} catch (Exception e) {
-				System.out.println("Error al leer el archivo: " + e.getMessage());
-			}
-		}
 		
-	static void leerArchivoTxt(String nombreArchivo) {
-		FileReader archivo = null;
-		BufferedReader lector = null;
-			try {
-				archivo = new FileReader(nombreArchivo);
-				lector = new BufferedReader(archivo);
-				String linea;
-				while ((linea = lector.readLine()) != null) {
-					System.out.println(linea);
-					}
-				} catch (Exception e) {
-					System.out.println("Error al leer el archivo: " + e.getMessage());
-					}
-			}
-		
-		static void guardarArbolDirectorio(File directorio, int nivel, OutputStreamWriter writer) {
-			if (directorio.isDirectory()) {
-				try {
-					writer.write("(D) " + directorio.getName() + " (Última modificación: " + obtenerFechaModificacion(directorio) + ")\n");
-					} catch (Exception e) {
-						System.out.println("Error: " + e.getMessage());
-						}
-				File[] contenido = directorio.listFiles();
-				Arrays.sort(contenido);
-				for (File archivo : contenido) {
-					if (archivo.isDirectory()) {
-						guardarArbolDirectorio(archivo, nivel + 1, writer);
-						} else {
-							try {
-								writer.write("(F) " + archivo.getName() + " (Última modificación: " + obtenerFechaModificacion(archivo) + ")\n");
-								} catch (Exception e) {
-									System.out.println("Error: " + e.getMessage());
-									}
-							}
-					}
-				}
-			}
-		
-		static String obtenerFechaModificacion(File archivo) {
-			SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			Date fecha = new Date(archivo.lastModified());
-			return formato.format(fecha);
-			}
-		
-}
