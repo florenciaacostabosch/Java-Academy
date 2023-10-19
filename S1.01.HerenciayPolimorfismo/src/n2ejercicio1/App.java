0734f789b574dc771221bec2e8ee9b0016a88745
florenciaacostabosch <florencia.acostabosch@gmail.com>
2023-10-19 10:08:22 +0200

    commit

---
+++ S1.01.HerenciayPolimorfismo/src/n2ejercicio1/App.java
@@ -0,0 +1,9 @@
+package n2ejercicio1;
+
+public class App {
+	public static void main(String[] args) {
+		Smartphone smartphone1 = new Smartphone("Samsung", "A30");
+		smartphone1.alarma();
+		smartphone1.fotografiar();
+	}
+}
---
+++ S1.01.HerenciayPolimorfismo/src/n2ejercicio1/Telefono.java
@@ -0,0 +1,15 @@
+package n2ejercicio1;
+
+public class Telefono {
+	 private String marca;
+	 private String modelo;
+	 
+	 public Telefono(String marca, String modelo) {
+		 this.marca = marca;
+		 this.modelo = modelo;
+		 }
+	 
+	 public void llamar(String numTelefono) {
+		 System.out.println("Se está llamando al número: "+ numTelefono);
+	 }
+}
